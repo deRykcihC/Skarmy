d2f3d3d87283f2035f2b225dc3cc800b9682536f
deRykcihC <wjihong21@gmail.com>
2025-12-07 17:50:53 +0800

    v1.1 - changed default gemini models - fixed some UI placement - added some text and buymeacoffee link

--- lib/widgets/screenshot_card.dart
+++ lib/widgets/screenshot_card.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:pixelshot_flutter/models/screenshot.dart';
 
@@ -22,7 +21,7 @@ class ScreenshotCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(16),
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.05),
+              color: Colors.black.withValues(alpha: 0.05),
               blurRadius: 10,
               offset: const Offset(0, 4),
             ),
